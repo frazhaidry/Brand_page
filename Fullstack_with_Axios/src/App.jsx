371a95cc8de782887a83896eda7fa036728aff49
frazhaidry <frazhaidry24@gmail.com>
2024-09-29 20:42:19 +0530

    CRUD_with_Axios

---
+++ Fullstack_with_Axios/src/App.jsx
@@ -0,0 +1,14 @@
+import { Posts } from "./componets/Posts"
+import  "./App.css"
+
+function App() {
+
+  
+  return (
+    <section className="main-section">
+      <Posts/>
+    </section>
+  )
+}
+
+export default App
